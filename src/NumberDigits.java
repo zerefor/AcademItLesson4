28767d54038f0b83caaf45835628ee6248b3e006
Zerefor <zerefor1@gmail.com>
2023-03-08 12:02:46 +0700

    Commit

--- src/NumberDigits.java
+++ src/NumberDigits.java
@@ -7,24 +7,27 @@ public class NumberDigits {
         System.out.println("Enter a number:");
         int number = scanner.nextInt();
 
-        int digitFromNumber;
+        if (number < 0) {
+            number = number * (-1);
+        }
+
         int digitsSum = 0;
         int oddDigitsSum = 0;
-        int findMaxDigit = 0;
+        int maxDigit = 0;
 
         while (number > 0) {
-            digitFromNumber = number % 10;
+            int digitFromNumber = number % 10;
             digitsSum += digitFromNumber;
 
             if (digitFromNumber % 2 != 0) {
                 oddDigitsSum += digitFromNumber;
             }
 
-            findMaxDigit = Math.max(digitFromNumber, findMaxDigit);
+            maxDigit = Math.max(digitFromNumber, maxDigit);
             number /= 10;
         }
 
-        System.out.println("The largest digit: " + findMaxDigit);
+        System.out.println("The largest digit: " + maxDigit);
         System.out.println("Sum of odd digits: " + oddDigitsSum);
         System.out.println("Sum of all digits: " + digitsSum);
     }
